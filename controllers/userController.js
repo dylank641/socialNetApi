22bca167698f626ebf643cdb048c3690e9395ceb
James Killelea <dylan.killelea@yahoo.com>
2022-05-16 20:16:10 -0500

    finished controllers and workind on models

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,3 +56,66 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     },
+    // delete a user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user was found related to this search.' });
+                    return;
+                }
+                return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+            }).then(dbUserData => {
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    addFriend({ params }, res) {
+        User.findOne({ _id: params.friendId })
+            .then(dbFriendData => {
+                if (!dbFriendData) {
+                    res.status(404).json({ message: 'No friend was found related to this search.' });
+                    return;
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { friends: params.friendId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No friend was found related to this search.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    deleteFriend({ params }, res) {
+        User.findOne({ _id: params.friendId })
+            .then(dbFriendData => {
+                console.log(dbFriendData)
+                if (!dbFriendData) {
+                    res.status(404).json({ message: 'No friend was found related to this search.' });
+                    return;
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { friends: params.friendId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No friend was found related to this search.' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    }
+
+};
+
+module.exports = userController;
